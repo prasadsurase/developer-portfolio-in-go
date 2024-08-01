87d4e311ab0f63f80dae90c59d84b2dc99a76bbf
Prasad Surase <prasad.surase@coupa.com>
2024-08-02 01:13:26 +0530

    Added basic http templates for home and about page

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 )
 
 const serverPort = ":8080"
@@ -21,10 +22,22 @@ func main() {
 
 // Home is the home page handler
 func Home(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(rw, "This is the home page")
+	renderTemplate(rw, "home.page.tmpl")
 }
 
 // About is the about page handler
 func About(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(rw, "This is the about page")
+	renderTemplate(rw, "about.page.tmpl")
+}
+
+func renderTemplate(rw http.ResponseWriter, tmpl string) {
+	parsedTemplate, err := template.ParseFiles("./templates/" + tmpl)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = parsedTemplate.Execute(rw, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
