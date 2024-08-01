d5d1aeaf338fb8ed97a8b78101924c1fb4f970d4
Prasad Surase <prasad.surase@coupa.com>
2024-08-02 02:13:24 +0530

    Added template cache to avoid templates from disk when called

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -6,15 +6,42 @@ import (
 	"net/http"
 )
 
+var templatesCache = make(map[string]*template.Template)
+
 // RenderTemplate is used to render the template
-func RenderTemplate(rw http.ResponseWriter, tmpl string) {
-	parsedTemplate, err := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
-	if err != nil {
-		fmt.Println(err)
+func RenderTemplate(rw http.ResponseWriter, tmplName string) {
+	var tmpl *template.Template
+	var err error
+
+	_, inMap := templatesCache[tmplName]
+	if !inMap {
+		fmt.Println("creating template and adding to cache")
+		err = createTemplateCache(tmplName)
+		if err != nil {
+			fmt.Printf("error creating template cache: %v\n", err)
+		}
+	} else {
+		fmt.Printf("using cached template: %v\n", tmplName)
 	}
 
-	err = parsedTemplate.Execute(rw, nil)
+	tmpl = templatesCache[tmplName]
+	err = tmpl.Execute(rw, nil)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("error executing template: %v\n", err)
 	}
+}
+
+// createTemplateCache creates the cache for templates using map
+func createTemplateCache(tmplName string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s", tmplName),
+		"./templates/base.layout.tmpl",
+	}
+
+	tmpl, err := template.ParseFiles(templates...)
+	if err != nil {
+		return err
+	}
+	templatesCache[tmplName] = tmpl
+	return nil
 }
