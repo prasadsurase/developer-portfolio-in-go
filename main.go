d7be2518886d18856ad176f3b82e9645ff18aec4
Prasad Surase <prasad.surase@coupa.com>
2024-08-02 01:26:51 +0530

    Moved http handlers to diff file

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"text/template"
 )
 
 const serverPort = ":8080"
@@ -17,27 +16,5 @@ func main() {
 	err := http.ListenAndServe(serverPort, nil)
 	if err != nil {
 		log.Fatal(err)
-	}
-}
-
-// Home is the home page handler
-func Home(rw http.ResponseWriter, r *http.Request) {
-	renderTemplate(rw, "home.page.tmpl")
-}
-
-// About is the about page handler
-func About(rw http.ResponseWriter, r *http.Request) {
-	renderTemplate(rw, "about.page.tmpl")
-}
-
-func renderTemplate(rw http.ResponseWriter, tmpl string) {
-	parsedTemplate, err := template.ParseFiles("./templates/" + tmpl)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	err = parsedTemplate.Execute(rw, nil)
-	if err != nil {
-		fmt.Println(err)
 	}
 }
