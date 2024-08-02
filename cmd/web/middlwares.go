9a193af9375f93cf43bd7230c263d2913738a1b0
Prasad Surase <prasad.surase@coupa.com>
2024-08-02 13:30:30 +0530

    Added middleware to handle CSRF

--- cmd/web/middlwares.go
+++ cmd/web/middlwares.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/justinas/nosurf"
 )
 
 func WriteToConsole(next http.Handler) http.Handler {
@@ -10,4 +12,16 @@ func WriteToConsole(next http.Handler) http.Handler {
 		fmt.Printf("Hit url: %v\n", r.URL)
 		next.ServeHTTP(rw, r)
 	})
+}
+
+func NoSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Secure:   false,
+		Path:     "/",
+		SameSite: http.SameSiteLaxMode,
+	})
+
+	return csrfHandler
 }
--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/go-chi/chi v1.5.5
 	github.com/go-chi/chi/v5 v5.1.0
 )
+
+require github.com/justinas/nosurf v1.1.1
