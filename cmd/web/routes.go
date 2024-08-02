b5bead650dfc7c005f9b4025d0bfa7cd58479add
Prasad Surase <prasad.surase@coupa.com>
2024-08-02 12:48:36 +0530

    Added another router

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,14 +3,19 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
 	"github.com/prasadsurase/developer-portfolio-in-go/config"
 	"github.com/prasadsurase/developer-portfolio-in-go/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// mux := pat.New()
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	mux := chi.NewRouter()
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
 	return mux
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/prasadsurase/developer-portfolio-in-go
 
 go 1.22.0
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+require (
+	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
+)
